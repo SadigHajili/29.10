108ac8acd0edc1128efffcfe24668fff018427be
Hajili <tu201806363@aztu.local>
2021-10-29 10:47:10 +0400

    first commit

---
+++ ConsoleApp8/ConsoleApp8/MyMathBase.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp8
+{
+    internal class MyMathBase
+    {
+        public void MYMATH()
+        {
+            int x = 5;
+            int y = 7;
+            Console.WriteLine(Math.Max(x, y));
+
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task1 
+            //Home ev1 = new Home(8, "Xirdalan");
+            //ev1.telefon = "+994123425526";
+            //ev1.unvan = "Heyder Aliyev 1";
+            //Console.WriteLine(ev1.Myhome());
+            #endregion
+            MyMath sadiq = new Math();
+            sadiq.MYMATH();
+            
+        }
+    }
+    public class Home
+    {
+
+        public string sheher;
+        public byte otaq;
+        public byte mertebe;
+        public string unvan;
+        public string telefon;
+
+        public Home(byte otaq, string sheher)
+        {
+            this.otaq = otaq;
+            this.sheher = sheher;
+        }
+        public string Myhome()
+        {
+
+            return $"Seher: {sheher}\nUnvan: {unvan}\nTelefon: {telefon}";
+
+
+        }
+    } 
+    
+
+    class MyMath
+    {
+        public void MYMATH(int eded, int quvvet)
+        {
+            for (int i = 0; i < quvvet; i++)
+            {
+                int x = 5;
+                int y = 7;
+                Console.WriteLine(Math.Max(x, y));
+            }
+        }
+    }
+}
+
+
+
+
+
+
